10287d0a8cf089a5908d6121cec5b28758de178b
Sangram Mohite <mohite.sangram@gmail.com>
2020-03-31 16:27:18 +0530

    Update

--- functions/utils.js
+++ functions/utils.js
@@ -1,8 +1,8 @@
 exports.unitActionCalorieMap = {
     "left-move": {
-        "low": 2,
-        "medium": 4,
-        "high": 6
+        "low": 3,
+        "medium": 5,
+        "high": 7
     },
     "right-move": {
         "low": 3,
@@ -20,9 +20,9 @@ exports.unitActionCalorieMap = {
         "high": 6
     },
     "running": {
-        "low": 4,
-        "medium": 8,
-        "high": 12
+        "low": 0.4,
+        "medium": 0.8,
+        "high": 1.2
     },
     "stop": {
         "low": 1,
@@ -42,7 +42,7 @@ exports.calculateCalories = (playerActionCounts, intensityLevel) => {
 
         }
     }
-    return calories;
+    return Math.round(calories);
 }
 
 
@@ -68,15 +68,15 @@ exports.unitActionFitnessPointsFactorMap = {
         "high": 0.7
     },
     "running": {
-        "low": 1.5,
-        "medium": 1.7,
-        "high": 2
+        "low": 0.25,
+        "medium": 0.50,
+        "high": 1
     },
 
     "stop": {
-        "low": 0.25,
-        "medium": 0.25,
-        "high": 0.25
+        "low": 0.1,
+        "medium": 0.1,
+        "high": 0.1
     }
 }
 
@@ -94,5 +94,5 @@ exports.calculateFitnessPoints = (duration, playerActionCounts, intensityLevel) 
 
     }
     fitnessPoints = totalActionCount * duration / 10;
-    return fitnessPoints;
+    return Math.round(fitnessPoints);
 }
