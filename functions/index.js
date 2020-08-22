b21c61d442d6d1f870d3ba944053c3bff65fe7a0
coder-pikachu <mohite.sangram@gmail.com>
2020-08-22 09:17:08 +0530

    NULL check for timestamp / updated database rules

--- functions/index.js
+++ functions/index.js
@@ -9,95 +9,95 @@ admin.initializeApp(functions.config().firebase);
 //  response.send("Hello from Firebase!");
 // });
 exports.processPlayerSessionData = functions.database.ref('/stage-bucket/player-sessions/{sessionId}')
-.onCreate((snapshot, context) => {
-    
-    const playerSessionData = snapshot.val();
-    
-    let userId = playerSessionData["user-id"];
-    let playerId = playerSessionData['player-id'];
-    let playerActivityRef = `/profiles/users/${userId}/players/${playerId}/activity-statistics`;
-    let playerDBRef = admin.database().ref(playerActivityRef);
-    let playerActivityStatistics;
-    playerDBRef.once('value').then(async (playerActivityStatisticsSnapshot) => {
-        
-        const duration = parseInt(playerSessionData["duration"]);
-        const intensityLevel = playerSessionData["intensity-level"];
-        const gameId = playerSessionData["game-id"];
-        
-        console.log(`Duration: ${duration}`);
-        
-        let calories = utils.calculateCalories(playerSessionData["player-action-counts"], intensityLevel);
-        let fitnessPoints = utils.calculateFitnessPoints(duration, playerSessionData["player-action-counts"], intensityLevel);
-        
-        playerActivityStatistics = playerActivityStatisticsSnapshot.val();
-        console.log(playerActivityStatistics)
-        
-        if (playerActivityStatistics === null) {
-            console.log("Player statistics null hence creating!");
-            playerActivityStatistics = {
-                "last-played":0,
-                "total-calories-burnt": 0,
-                "total-duration": 0,
-                "total-fitness-points": 0,
-                "redeemed-fitness-points": 0,
-                "games-statistics": {
-                    
+    .onCreate((snapshot, context) => {
+
+        const playerSessionData = snapshot.val();
+
+        let userId = playerSessionData["user-id"];
+        let playerId = playerSessionData['player-id'];
+        let playerActivityRef = `/profiles/users/${userId}/players/${playerId}/activity-statistics`;
+        let playerDBRef = admin.database().ref(playerActivityRef);
+        let playerActivityStatistics;
+        playerDBRef.once('value').then(async (playerActivityStatisticsSnapshot) => {
+
+            const duration = parseInt(playerSessionData["duration"]);
+            const intensityLevel = playerSessionData["intensity-level"];
+            const gameId = playerSessionData["game-id"];
+            const lastPlayedTimestamp = (playerSessionData["timestamp"]) ? (playerSessionData["timestamp"]) : null;
+            console.log(`Duration: ${duration}`);
+
+            let calories = utils.calculateCalories(playerSessionData["player-action-counts"], intensityLevel);
+            let fitnessPoints = utils.calculateFitnessPoints(duration, playerSessionData["player-action-counts"], intensityLevel);
+
+            playerActivityStatistics = playerActivityStatisticsSnapshot.val();
+            console.log(playerActivityStatistics)
+
+            if (playerActivityStatistics === null) {
+                console.log("Player statistics null hence creating!");
+                playerActivityStatistics = {
+                    "last-played": 0,
+                    "total-calories-burnt": 0,
+                    "total-duration": 0,
+                    "total-fitness-points": 0,
+                    "redeemed-fitness-points": 0,
+                    "games-statistics": {
+
+                    }
+
                 }
-                
+                playerActivityStatistics["games-statistics"][gameId] = null;
             }
-            playerActivityStatistics["games-statistics"][gameId] = null;
-        }
-        
-        console.log(`Calories: ${calories} and Fitness Points:${fitnessPoints}`);
-        playerActivityStatistics["last-played"] = (playerSessionData["timestamp"]);
-        playerActivityStatistics["total-calories-burnt"] += calories;
-        playerActivityStatistics["total-duration"] += (duration);
-        playerActivityStatistics["total-fitness-points"] += fitnessPoints;
-        
-        if (playerActivityStatistics["games-statistics"][gameId]) {
-            playerActivityStatistics["games-statistics"][gameId]["last-played"] = (playerSessionData["timestamp"]);
-            playerActivityStatistics["games-statistics"][gameId]["calories-burnt"] += calories;
-            playerActivityStatistics["games-statistics"][gameId]["duration"] += (duration);
-            playerActivityStatistics["games-statistics"][gameId]["fitness-points"] += fitnessPoints;
-            playerActivityStatistics["games-statistics"][gameId]["game-points"] += parseInt(playerSessionData["points"]);
-            
-        } else {
-            playerActivityStatistics["games-statistics"][gameId] = {
-                "last-played":(playerSessionData["timestamp"]),
-                "calories-burnt": calories,
-                "duration": duration,
-                "fitness-points": fitnessPoints,
-                "game-points": parseInt(playerSessionData["points"]),
-                
+
+            console.log(`Calories: ${calories} and Fitness Points:${fitnessPoints}`);
+            playerActivityStatistics["last-played"] = lastPlayedTimestamp;
+            playerActivityStatistics["total-calories-burnt"] += calories;
+            playerActivityStatistics["total-duration"] += (duration);
+            playerActivityStatistics["total-fitness-points"] += fitnessPoints;
+
+            if (playerActivityStatistics["games-statistics"][gameId]) {
+                playerActivityStatistics["games-statistics"][gameId]["last-played"] = lastPlayedTimestamp;
+                playerActivityStatistics["games-statistics"][gameId]["calories-burnt"] += calories;
+                playerActivityStatistics["games-statistics"][gameId]["duration"] += (duration);
+                playerActivityStatistics["games-statistics"][gameId]["fitness-points"] += fitnessPoints;
+                playerActivityStatistics["games-statistics"][gameId]["game-points"] += parseInt(playerSessionData["points"]);
+
+            } else {
+                playerActivityStatistics["games-statistics"][gameId] = {
+                    "last-played": lastPlayedTimestamp,
+                    "calories-burnt": calories,
+                    "duration": duration,
+                    "fitness-points": fitnessPoints,
+                    "game-points": parseInt(playerSessionData["points"]),
+
+                }
             }
-        }
-        if(playerSessionData["game-data"])
-        playerActivityStatistics["games-statistics"][gameId]["game-data"] = playerSessionData["game-data"];
+            if (playerSessionData["game-data"])
+                playerActivityStatistics["games-statistics"][gameId]["game-data"] = playerSessionData["game-data"];
+            console.log(playerActivityStatistics);
+
+            const playerSessionDataToUpdate = snapshot.toJSON();
+            console.log(playerSessionDataToUpdate);
+            playerSessionDataToUpdate["calories"] = calories;
+            playerSessionDataToUpdate["fitness-points"] = fitnessPoints;
+
+
+            let newSessionRef = admin.database().ref('/sessions/game-sessions').push();
+            let newSessionPath = `/sessions/game-sessions/${newSessionRef.key}`;
+            let updatePayload = {
+            }
+            updatePayload[newSessionPath] = playerSessionDataToUpdate;
+            updatePayload[playerActivityRef] = playerActivityStatistics;
+            console.log(updatePayload);
+            await admin.database().ref().update(updatePayload);
+            return snapshot.ref.remove();
+
+        }).catch(exception => {
+            console.error(exception);
+        });
+        // Grab the current value of what was written to the Realtime Database
+
+
+
         console.log(playerActivityStatistics);
-        
-        const playerSessionDataToUpdate = snapshot.toJSON();
-        console.log(playerSessionDataToUpdate);
-        playerSessionDataToUpdate["calories"] = calories;
-        playerSessionDataToUpdate["fitness-points"] = fitnessPoints;
-        
-        
-        let newSessionRef = admin.database().ref('/sessions/game-sessions').push();
-        let newSessionPath = `/sessions/game-sessions/${newSessionRef.key}`;
-        let updatePayload = {
-        }
-        updatePayload[newSessionPath] = playerSessionDataToUpdate;
-        updatePayload[playerActivityRef] = playerActivityStatistics;
-        console.log(updatePayload);
-        await admin.database().ref().update(updatePayload);
-        return snapshot.ref.remove();
-        
-    }).catch(exception => {
-        console.error(exception);
+
     });
-    // Grab the current value of what was written to the Realtime Database
-    
-    
-    
-    console.log(playerActivityStatistics);
-    
-});
