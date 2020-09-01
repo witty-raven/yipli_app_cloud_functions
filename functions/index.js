d63eb20409359d6ddb010a2d0b1648c2e58cd260
Ameet Palkar <ameetpalkar1@gmail.com>
2020-09-02 00:45:37 +0530

    Familification stats calculation

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,8 @@
+/* eslint-disable no-extra-boolean-cast */
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
-const utils = require('./utils');
-const playerSessionModel = require('./models/playerSessionModel')
+//const utils = require('./utils');
+const playerSessionDataModelModel = require('./models/playerSessionModel')
 admin.initializeApp(functions.config().firebase);
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -9,87 +10,66 @@ admin.initializeApp(functions.config().firebase);
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
-exports.processPlayerSessionData = functions.database.ref('/stage-bucket/player-sessions/{sessionId}')
-    .onCreate((snapshot, context) => {
 
-        const playerSessionDataJSON = snapshot.val();
-        const playerSessionDataModel = playerSessionModel.from(playerSessionDataJSON);
+exports.processplayerSessionDataModel = functions.database.ref('/stage-bucket/player-sessions/{sessionId}')
+    .onCreate((snapshot) => {
+
+        const playerSessionDataModelJSON = snapshot.val();
+        const playerSessionDataModel = playerSessionDataModelModel.fromJSON(playerSessionDataModelJSON);
 
 
-        let playerActivityRef = `/profiles/users/${userId}/players/${playerId}/activity-statistics`;
+        let playerActivityRef = `/profiles/users/${playerSessionDataModel.userId}/players/${playerSessionDataModel.playerId}/activity-statistics`;
+
         let playerDBRef = admin.database().ref(playerActivityRef);
         let playerActivityStatistics;
+
         playerDBRef.once('value').then(async (playerActivityStatisticsSnapshot) => {
 
-            const duration = parseInt(playerSessionData["duration"]);
-            const intensityLevel = playerSessionData["intensity-level"];
-            const gameId = playerSessionData["game-id"];
-            const lastPlayedTimestamp = (playerSessionData["timestamp"]) ? (playerSessionData["timestamp"]) : null;
-            console.log(`Duration: ${duration}`);
+            let updatePayload = {
+            }
 
-            let calories = utils.calculateCalories(playerSessionData["player-action-counts"], intensityLevel);
-            let fitnessPoints = utils.calculateFitnessPoints(duration, playerSessionData["player-action-counts"], intensityLevel);
-
+            const lastPlayedTimestamp = (playerSessionDataModel.timestamp);
+            console.log(`lastPlayedTimestamp: ${lastPlayedTimestamp}`);
+            if (lastPlayedTimestamp === null) throw ("LAST TIME STAMP NOT SET! Please set the last played timestamp!");
             playerActivityStatistics = playerActivityStatisticsSnapshot.val();
             console.log(playerActivityStatistics)
 
             if (playerActivityStatistics === null) {
                 console.log("Player statistics null hence creating!");
-                playerActivityStatistics = {
-                    "last-played": 0,
-                    "total-calories-burnt": 0,
-                    "total-duration": 0,
-                    "total-fitness-points": 0,
-                    "redeemed-fitness-points": 0,
-                    "games-statistics": {
-
-                    }
-
-                }
-                playerActivityStatistics["games-statistics"][gameId] = null;
+                playerActivityStatistics = buildNewActivityStatistics(playerActivityStatistics, playerSessionDataModel);
             }
 
-            console.log(`Calories: ${calories} and Fitness Points:${fitnessPoints}`);
-            playerActivityStatistics["last-played"] = lastPlayedTimestamp;
-            playerActivityStatistics["total-calories-burnt"] += calories;
-            playerActivityStatistics["total-duration"] += (duration);
-            playerActivityStatistics["total-fitness-points"] += fitnessPoints;
+            console.log(`Calories: ${playerSessionDataModel.calories} and Fitness Points:${playerSessionDataModel.fitnessPoints}`);
+            setActivityStatsDataFromModel(playerActivityStatistics, lastPlayedTimestamp, playerSessionDataModel);
 
-            if (playerActivityStatistics["games-statistics"][gameId]) {
-                playerActivityStatistics["games-statistics"][gameId]["last-played"] = lastPlayedTimestamp;
-                playerActivityStatistics["games-statistics"][gameId]["calories-burnt"] += calories;
-                playerActivityStatistics["games-statistics"][gameId]["duration"] += (duration);
-                playerActivityStatistics["games-statistics"][gameId]["fitness-points"] += fitnessPoints;
-                playerActivityStatistics["games-statistics"][gameId]["game-points"] += parseInt(playerSessionData["points"]);
+            setGameActivityDataFromModel(playerActivityStatistics, playerSessionDataModel, lastPlayedTimestamp);
 
-            } else {
-                playerActivityStatistics["games-statistics"][gameId] = {
-                    "last-played": lastPlayedTimestamp,
-                    "calories-burnt": calories,
-                    "duration": duration,
-                    "fitness-points": fitnessPoints,
-                    "game-points": parseInt(playerSessionData["points"]),
-
-                }
-            }
-            if (playerSessionData["game-data"])
-                playerActivityStatistics["games-statistics"][gameId]["game-data"] = playerSessionData["game-data"];
-            console.log(playerActivityStatistics);
-
-            const playerSessionDataToUpdate = snapshot.toJSON();
-            console.log(playerSessionDataToUpdate);
-            playerSessionDataToUpdate["calories"] = calories;
-            playerSessionDataToUpdate["fitness-points"] = fitnessPoints;
+            const playerSessionDataModelToUpdate = snapshot.toJSON();
+            console.log(playerSessionDataModelToUpdate);
+            playerSessionDataModelToUpdate["calories"] = playerSessionDataModel.calories;
+            playerSessionDataModelToUpdate["fitness-points"] = playerSessionDataModel.fitnessPoints;
 
 
             let newSessionRef = admin.database().ref('/sessions/game-sessions').push();
             let newSessionPath = `/sessions/game-sessions/${newSessionRef.key}`;
-            let updatePayload = {
-            }
-            updatePayload[newSessionPath] = playerSessionDataToUpdate;
+
+
+
+            //TODO: Add changes for weekly/monthly
+            // /user-statistics/<<USER_ID>>/performance-statistics/Weekly/2020/45/playerData/<<PLAYER_ID>>
+            var { weeklyStatsRef, weeklyStatsDataToUpdate } = await processWeeklyStatisticsData(playerSessionDataModel);
+            var { monthlyStatsRef, monthStatsDataToUpdate } = await processMonthlyStatisticsData(playerSessionDataModel);
+
+
+            updatePayload[newSessionPath] = playerSessionDataModelToUpdate;
             updatePayload[playerActivityRef] = playerActivityStatistics;
+            updatePayload[weeklyStatsRef] = weeklyStatsDataToUpdate;
+            updatePayload[monthlyStatsRef] = monthStatsDataToUpdate;
+
             console.log(updatePayload);
             await admin.database().ref().update(updatePayload);
+
+
             return snapshot.ref.remove();
 
         }).catch(exception => {
@@ -102,3 +82,101 @@ exports.processPlayerSessionData = functions.database.ref('/stage-bucket/player-
         console.log(playerActivityStatistics);
 
     });
+
+async function processWeeklyStatisticsData(playerSessionDataModel) {
+    var weeklyStatsRef = admin.database().ref(`/user-statistics/${playerSessionDataModel.userId}/performance-statistics/weekly/${playerSessionDataModel.getWeekYear()}/${playerSessionDataModel.getWeek()}/playerData/${playerSessionDataModel.playerId}`);
+    var currentWeeklyStatsDataSnapshot = await weeklyStatsRef.once('value');
+    var weeklyStatsDataToUpdate;
+
+    if (currentWeeklyStatsDataSnapshot) {
+        let currentWeeklyStatsData = currentWeeklyStatsDataSnapshot.val();
+        if (!!currentWeeklyStatsData) {
+            weeklyStatsDataToUpdate = {
+                "fitnessPoints": playerSessionDataModel.fitnessPoints,
+                "calories": playerSessionDataModel.calories,
+                "dayPunch": {}
+            };
+        }
+        else {
+            weeklyStatsDataToUpdate = {
+                "fitnessPoints": currentWeeklyStatsData["fitnessPoints"] + playerSessionDataModel.fitnessPoints,
+                "calories": currentWeeklyStatsData["calories"] + playerSessionDataModel.calories,
+                "dayPunch": currentWeeklyStatsData["dayPunch"]
+            };
+        }
+        weeklyStatsDataToUpdate.dayPunch[playerSessionDataModel.getDayOfTheWeek()] = 1;
+        weeklyStatsDataToUpdate["totalDaysPunched"] = weeklyStatsDataToUpdate.dayPunch.length;
+    }
+    return { weeklyStatsRef, weeklyStatsDataToUpdate };
+}
+
+async function processMonthlyStatisticsData(playerSessionDataModel) {
+    var monthlyStatsRef = admin.database().ref(`/user-statistics/${playerSessionDataModel.userId}/performance-statistics/monthly/${playerSessionDataModel.getMonthYear()}/${playerSessionDataModel.getMonth()}/playerData/${playerSessionDataModel.playerId}`);
+    var currentMonthlyStatsDataSnapshot = await monthlyStatsRef.once('value');
+    var monthlyStatsDataToUpdate;
+
+    if (currentMonthlyStatsDataSnapshot) {
+        let currentMonthlyStatsData = currentMonthlyStatsDataSnapshot.val();
+        if (!!currentMonthlyStatsData) {
+            monthlyStatsDataToUpdate = {
+                "fitnessPoints": playerSessionDataModel.fitnessPoints,
+                "calories": playerSessionDataModel.calories,
+                "dayPunch": {}
+            };
+        }
+        else {
+            monthlyStatsDataToUpdate = {
+                "fitnessPoints": currentMonthlyStatsData["fitnessPoints"] + playerSessionDataModel.fitnessPoints,
+                "calories": currentMonthlyStatsData["calories"] + playerSessionDataModel.calories,
+                "dayPunch": currentMonthlyStatsData["dayPunch"]
+            };
+        }
+        monthlyStatsDataToUpdate.dayPunch[playerSessionDataModel.getMonth()] = 1;
+        monthlyStatsDataToUpdate["totalDaysPunched"] = monthlyStatsDataToUpdate.dayPunch.length;
+    }
+    return { monthlyStatsRef, monthlyStatsDataToUpdate };
+}
+
+function setGameActivityDataFromModel(playerActivityStatistics, playerSessionDataModel, lastPlayedTimestamp) {
+    if (playerActivityStatistics["games-statistics"][playerSessionDataModel.gameId]) {
+        playerActivityStatistics["games-statistics"][playerSessionDataModel.gameId]["last-played"] = lastPlayedTimestamp;
+        playerActivityStatistics["games-statistics"][playerSessionDataModel.gameId]["calories-burnt"] += playerSessionDataModel.calories;
+        playerActivityStatistics["games-statistics"][playerSessionDataModel.gameId]["duration"] += (playerSessionDataModel.duration);
+        playerActivityStatistics["games-statistics"][playerSessionDataModel.gameId]["fitness-points"] += playerSessionDataModel.fitnessPoints;
+        playerActivityStatistics["games-statistics"][playerSessionDataModel.gameId]["game-points"] += playerSessionDataModel.gameData.highScore ? (playerSessionDataModel.gameData.highScore) : 0;
+
+    }
+    else {
+        playerActivityStatistics["games-statistics"][playerSessionDataModel.gameId] = {
+            "last-played": lastPlayedTimestamp,
+            "calories-burnt": playerSessionDataModel.calories,
+            "duration": playerSessionDataModel.duration,
+            "fitness-points": playerSessionDataModel.fitnessPoints,
+            "game-points": playerSessionDataModel.gameData.highScore ? (playerSessionDataModel.gameData.highScore) : 0,
+        };
+    }
+    if (playerSessionDataModel.gameData)
+        playerActivityStatistics["games-statistics"][playerSessionDataModel.gameId]["game-data"] = playerSessionDataModel.gameData;
+    console.log(playerActivityStatistics);
+}
+
+function setActivityStatsDataFromModel(playerActivityStatistics, lastPlayedTimestamp, playerSessionDataModel) {
+    playerActivityStatistics["last-played"] = lastPlayedTimestamp;
+    playerActivityStatistics["total-calories-burnt"] += playerSessionDataModel.calories;
+    playerActivityStatistics["total-duration"] += (playerSessionDataModel.duration);
+    playerActivityStatistics["total-fitness-points"] += playerSessionDataModel.fitnessPoints;
+}
+
+function buildNewActivityStatistics(playerActivityStatistics, playerSessionDataModel) {
+    playerActivityStatistics = {
+        "last-played": 0,
+        "total-calories-burnt": 0,
+        "total-duration": 0,
+        "total-fitness-points": 0,
+        "redeemed-fitness-points": 0,
+        "games-statistics": {}
+    };
+    playerActivityStatistics["games-statistics"][playerSessionDataModel.gameId] = null;
+    return playerActivityStatistics;
+}
+
--- functions/models/playerSessionModel.js
+++ functions/models/playerSessionModel.js
@@ -1,26 +1,104 @@
+/* eslint-disable no-extra-boolean-cast */
 module.exports = class PlayerSessionDataModel {
-    fromJSON(json) {
-        this.age = json["age"] || "";
-        this.duration = json["duration"] || "";
-        this.endTime = json["end-time"] || "";
-        this.gameId = json["game-id"] || "";
-        this.intensityLevel = json["intensity-level"] || "";
-        this.matId = json["mat-id"] || "";
-        this.playerHeight = json["player-height"] || "";
-        this.playerId = json["player-id"] || "";
-        this.points = json["points"] || "";
-        this.startTime = json["start-time"] || "";
-        this.userId = json["user-id"] || "";
-        if (!!json["game-data"]) {
-            this.gameData = {};
-            this.gameData.highScore = json["game-data"]["high-score"] || "";
-            this.gameData.moneyEarned = json["game-data"]["money-earned"] || "";
+    constructor(modelData) {
+        this.age = modelData["age"];
+        this.duration = modelData["duration"];
+        this.endTime = modelData["endTime"];
+        this.gameId = modelData["gameId"];
+        this.intensityLevel = modelData["intensityLevel"];
+        this.matId = modelData["matId"];
+        this.playerHeight = modelData["playerHeight"];
+        this.playerId = modelData["playerId"];
+        this.points = modelData["points"];
+        this.startTime = modelData["startTime"];
+        this.userId = modelData["userId"];
+        this.gameData = modelData["gameData"] || {};
+        this.playerActionCounts = modelData["playerActionCounts"] || {};
+        this.timestamp = modelData["timestamp"] ? modelData["timestamp"] : null;
+        if(this.timestamp)
+            this.timestampDate = new Date(this.timestamp);
+        else 
+            this.timestampDate = null;
+
+        this.calories = modelData["calories"];
+        this.fitnessPoints = modelData.fitnessPoints;
+    }
+
+    getYear() {
+        if (this.timestampDate) {
+            return this.timestampDate.getFullYear();
         }
+    }
+
+    // This script is released to the public domain and may be used, modified and
+    // distributed without restrictions. Attribution not necessary but appreciated.
+    // Source: https://weeknumber.net/how-to/javascript
+
+    // Returns the ISO week of the date.
+    getWeek() {
+        
+        this.timestampDate.setHours(0, 0, 0, 0);
+        // Thursday in current week decides the year.
+        this.timestampDate.setDate(this.timestampDate.getDate() + 3 - (this.timestampDate.getDay() + 6) % 7);
+        // January 4 is always in week 1.
+        var week1 = new Date(this.timestampDate.getFullYear(), 0, 4);
+        // Adjust to Thursday in week 1 and count number of weeks from date to week1.
+        return 1 + Math.round(((this.timestampDate.getTime() - week1.getTime()) / 86400000 - 3 + (week1.getDay() + 6) % 7) / 7);
+    }
+
+    getDayOfTheWeek(){
+        return this.timestampDate.getDay();
+    }
+    // Returns the four-digit year corresponding to the ISO week of the date.
+    getWeekYear() {
+        this.timestampDate.setDate(this.timestampDate.getDate() + 3 - (this.timestampDate.getDay() + 6) % 7);
+        return this.timestampDate.getFullYear();
+    }
+
+    getMonth(){
+        return this.timestampDate.getMonth();
+    }
+
+    static fromJSON(json) {
+        var modelData = {};
+
+        modelData.age = json["age"] || 0;
+        modelData.duration = json["duration"] || 0;
+        modelData.endTime = json["end-time"] || "";
+        modelData.gameId = json["game-id"] || "";
+        modelData.intensityLevel = json["intensity-level"] || "";
+        modelData.matId = json["mat-id"] || "";
+        modelData.playerHeight = json["player-height"] || "";
+        modelData.playerId = json["player-id"] || "";
+        modelData.points = json["points"] || "";
+        modelData.startTime = json["start-time"] || "";
+        modelData.userId = json["user-id"] || "";
+        modelData.fitnessPoints = json["fitness-points"] || 0;
+        modelData.calories = json["calories"] || 0;
+
+        modelData.gameData = null;
+        if (!!json["game-data"]) {
+            modelData.gameData = {};
+            try {
+                modelData.gameData.highScore = parseInt(json["game-data"]["high-score"]) || 0;
+                modelData.gameData.moneyEarned = parseInt(json["game-data"]["money-earned"]) || 0;
+            } catch (e) {
+                modelData.gameData = {
+                    highScore: 0,
+                    moneyEarned: 0
+                }
+            }
+        }
+
+        modelData.playerActionCounts = null;
         if (!!json["player-action-counts"]) {
-            this.playerActionCounts = {};
-            this.playerActionCounts = json["player-action-counts"];
+            modelData.playerActionCounts = {};
+            modelData.playerActionCounts = json["player-action-counts"];
             //TODO: Need to check if the actions need to be listed.
         }
+        modelData["timestamp"] = json["timestamp"];
+
+        return new PlayerSessionDataModel(modelData);
     }
 }
 
