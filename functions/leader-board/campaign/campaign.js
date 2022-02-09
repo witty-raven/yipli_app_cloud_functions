5c50e0202f35e48bee1eaf8d9739e8e8839e7062
Komal <komallaghate@gmail.com>
2022-02-09 18:38:55 +0530

    Campaign family dependency handling

--- functions/leader-board/campaign/campaign.js
+++ functions/leader-board/campaign/campaign.js
@@ -1,28 +1,32 @@
 const firebase = require("firebase-admin");
 const UTILITY = require("../utility/utility");
-
 const REQUEST = "Campaign Leader Board"
-
 const getCampaignResult = async (query) => {
     const campaignId = query.campaignId;
-
     var refToResult = firebase.database().ref(UTILITY.paths["pathToLeaderBoards"] + "campaign/" + campaignId);
     var refToInventory = firebase.database().ref(UTILITY.paths["pathToLeaderBoardsInventory"] + "campaign/" + campaignId)
     var campaignInfo = await refToInventory.once("value");
     var refToTeamplates = firebase.database().ref(UTILITY.paths["pathToLeaderBoardsInventory"] + "templates/" + campaignInfo.child("template").val());
     var template = await refToTeamplates.once("value");
-
     var campaign = campaignInfo.val();
     campaign.template = template.val();
     campaign["banner-url"] = UTILITY.constructPublicURL(UTILITY.baseURLs["storageURL"], campaign["banner-url"], UTILITY.mediaType["media"]);
-
     return new Promise((resolve, reject) => {
         refToResult.once("value").then(itemList => {
             var List = [];
             itemList.forEach(item => {
-                var obj = item.val()
-                obj["display-img-url"] = UTILITY.constructPublicURL(UTILITY.baseURLs["storageURL"], obj["display-img-url"], UTILITY.mediaType["media"]);
+                var obj ={};
                 obj.id = item.key;
+                obj["display-img-url"] = UTILITY.constructPublicURL(UTILITY.baseURLs["storageURL"], item.child("display-img-url").val(), UTILITY.mediaType["media"]);
+                obj["display-name"] = item.child("display-name").val();
+                obj["family-name"] = item.child("family-name").val();
+                obj["count"] = 0;
+                if(campaign["contestant-type"] === "family"){
+                    item.child("players").forEach(player => {
+                        obj["count"] += player.val()["count"];
+                    });
+                }
+                else obj.count = item.child("count").val();
                 List.push(obj);
             })
             campaign.result = List;
@@ -30,11 +34,9 @@ const getCampaignResult = async (query) => {
         })
     })
 }
-
 const getCampaignList = async (query) => {
     return new Promise((resolve, reject) => resolve("working " + REQUEST))
 }
-
 const postSession = async (query) => {
     var session = query;
     var activeCampaign = await getActiveCampaigns(session.timestamp);
@@ -47,13 +49,8 @@ const postSession = async (query) => {
     })
     return new Promise((resolve, reject) => resolve(activeCampaign))
 }
-
 const createNew = async (query) => {
-
 };
-
-
-
 async function getActiveCampaigns(timestamp) {
     if (timestamp.campaignId == 0) timestamp = new Date().getTime();
     var activeCampaigns = [];
@@ -76,51 +73,89 @@ async function fetchTemplate(templateId) {
 }
 async function addEntryToLeaderBoard(session, campaign, template) {
     var entryId = (campaign["contestant-type"] === "family") ? session["user-id"] : session["player-id"];
+    var game = await firebase.database().ref(UTILITY.paths["pathToGameInventory"] + session["game-id"]).once("value");
+    if (game.mode === "multiplayer") {
+        entryId = session["user-id"] + "/" + session["player-id"];
+    }
     var entryExists = await firebase.database().ref(UTILITY.paths["pathToLeaderBoards"] + "campaign/" + campaign.id + "/" + entryId).once("value");
     if (entryExists.val()) {
         var entry = entryExists.val();
         updateLeaderBoardEntry(session, campaign, template, entryId, entry);
     }
     else createLeaderBoardEntry(session, campaign, template, entryId);
-
 }
 async function createLeaderBoardEntry(session, campaign, template, entryId) {
     var entry = {};
-    console.log(session[template["compete-metric"].type][template["compete-metric"].action]);
-    if (template["compete-metric"].type === "player-actions") entry["count"] = session[template["compete-metric"].type][template["compete-metric"].action];
-    else entry["count"] = session[template["compete-metric"].type];
-    entry["timestamp"] = session.timestamp;
-    fetchEntryDetails(session, campaign["contestant-type"]).then(details => {
-        entry["family-name"] = details["family-name"];
-        entry["display-name"] = details["display-name"];
-        entry["display-img-url"] = details["display-img-url"];
-        firebase.database().ref(UTILITY.paths["pathToLeaderBoards"] + "campaign/" + campaign.id + "/" + entryId).set(entry);
-    })
+    if (campaign["contestant-type"] === "family") {
+        entry["players"] = {};
+        entry["players"][session["player-id"]] = {};
+        entry["players"][session["player-id"]]["count"] = 0;
+        if (template["compete-metric"].type === "player-actions") entry["players"][session["player-id"]]["count"] = session[template["compete-metric"].type][template["compete-metric"].action];
+        else entry["players"][session["player-id"]]["count"] = session[template["compete-metric"].type];
+        entry["players"][session["player-id"]]["timestamp"] = session.timestamp;
+        fetchEntryDetails(session, campaign["contestant-type"]).then(details => {
+            entry["family-name"] = details["family-name"];
+            entry["display-name"] = details["display-name"];
+            entry["display-img-url"] = details["display-img-url"];
+            firebase.database().ref(UTILITY.paths["pathToLeaderBoards"] + "campaign/" + campaign.id + "/" + entryId).set(entry);
+        })
+    }
+    else {
+        if (template["compete-metric"].type === "player-actions") entry[session["player-id"]]["count"] = session[template["compete-metric"].type][template["compete-metric"].action];
+        else entry["count"] = session[template["compete-metric"].type];
+        entry["timestamp"] = session.timestamp;
+        fetchEntryDetails(session, campaign["contestant-type"]).then(details => {
+            entry["family-name"] = details["family-name"];
+            entry["display-name"] = details["display-name"];
+            entry["display-img-url"] = details["display-img-url"];
+            firebase.database().ref(UTILITY.paths["pathToLeaderBoards"] + "campaign/" + campaign.id + "/" + entryId).set(entry);
+        })
+    }
 }
 async function updateLeaderBoardEntry(session, campaign, template, entryId, entry) {
-    if (template["compete-metric"]["in-one-life"]) {
-        if (entry["count"] < session[template["compete-metric"].type]) {
-            if (template["compete-metric"].type === "player-actions") entry["count"] = session[template["compete-metric"].type][template["compete-metric"].action];
-            else entry["count"] = session[template["compete-metric"].type];
-            entry["timestamp"] = session.timestamp;
+    if(campaign["contestant-type"] === "family"){
+        if(!entry["players"][session["player-id"]]) {
+            entry["players"][session["player-id"]] = {};
+            entry["players"][session["player-id"]]["count"] = 0;
+        }
+        if (template["compete-metric"]["in-one-life"]) {
+            if (entry["players"][session["player-id"]]["count"] < session[template["compete-metric"].type]) {
+                if (template["compete-metric"].type === "player-actions") entry["players"][session["player-id"]]["count"] = session[template["compete-metric"].type][template["compete-metric"].action];
+                else entry["players"][session["player-id"]]["count"] = session[template["compete-metric"].type];
+                entry["players"][session["player-id"]]["timestamp"] = session.timestamp;
+                firebase.database().ref(UTILITY.paths["pathToLeaderBoards"] + "campaign/" + campaign.id + "/" + entryId).set(entry);
+            }
+        }
+        else {
+            if (template["compete-metric"].type === "player-actions") entry["players"][session["player-id"]]["count"] = entry["players"][session["player-id"]]["count"] + session[template["compete-metric"].type][template["compete-metric"].action];
+            else entry["players"][session["player-id"]]["count"] = entry["players"][session["player-id"]]["count"] + session[template["compete-metric"].type];
+            entry["players"][session["player-id"]]["timestamp"] = session.timestamp;
             firebase.database().ref(UTILITY.paths["pathToLeaderBoards"] + "campaign/" + campaign.id + "/" + entryId).set(entry);
         }
     }
-    else {
-        if (template["compete-metric"].type === "player-actions") entry["count"] = entry["count"] +  session[template["compete-metric"].type][template["compete-metric"].action];
-        else entry["count"] = entry["count"] + session[template["compete-metric"].type];
-        entry["timestamp"] = session.timestamp;
-        firebase.database().ref(UTILITY.paths["pathToLeaderBoards"] + "campaign/" + campaign.id + "/" + entryId).set(entry);
+    else{
+        if (template["compete-metric"]["in-one-life"]) {
+            if (entry["count"] < session[template["compete-metric"].type]) {
+                if (template["compete-metric"].type === "player-actions") entry["count"] = session[template["compete-metric"].type][template["compete-metric"].action];
+                else entry["count"] = session[template["compete-metric"].type];
+                entry["timestamp"] = session.timestamp;
+                firebase.database().ref(UTILITY.paths["pathToLeaderBoards"] + "campaign/" + campaign.id + "/" + entryId).set(entry);
+            }
+        }
+        else {
+            if (template["compete-metric"].type === "player-actions") entry["count"] = entry["count"] + session[template["compete-metric"].type][template["compete-metric"].action];
+            else entry["count"] = entry["count"] + session[template["compete-metric"].type];
+            entry["timestamp"] = session.timestamp;
+            firebase.database().ref(UTILITY.paths["pathToLeaderBoards"] + "campaign/" + campaign.id + "/" + entryId).set(entry);
+        }
     }
 }
 async function fetchEntryDetails(session, contestantType) {
     var userId = session["user-id"];
     var playerId = session["player-id"];
-
     var details = {};
     var userName = await firebase.database().ref(UTILITY.paths["pathToUser"] + userId + "/display-name").once("value");
     details["family-name"] = userName.val();
-
     if (contestantType === "family") {
         var familyProfilePic = await firebase.database().ref(UTILITY.paths["pathToUser"] + userId + "/profile-pic-url").once("value");
         details["display-name"] = userName.val();
@@ -128,7 +163,6 @@ async function fetchEntryDetails(session, contestantType) {
         else details["display-img-url"] = "profile-pics/placeholder_image.png";
         return details;
     }
-
     var playerName = await firebase.database().ref(UTILITY.paths["pathToUser"] + userId + "/players/" + playerId + "/name").once("value");
     var playerProfileImg = await firebase.database().ref(UTILITY.paths["pathToUser"] + userId + "/players/" + playerId + "/profile-pic-url").once("value")
     details["display-name"] = playerName.val();
@@ -136,7 +170,6 @@ async function fetchEntryDetails(session, contestantType) {
     else details["display-img-url"] = "profile-pics/placeholder_image.png";
     return details;
 }
-
 exports.request = {
     'list': getCampaignList,
     'result': getCampaignResult,
